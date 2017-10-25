149216c546b6fb456cb7ffb955cdeaf8e0e99680
Christian <ChrisWcs@users.noreply.github.com>
2017-10-25 13:37:59 -0600

    Fill out reducer

---
+++ src/reducers/initialState.js
@@ -0,0 +1,8 @@
+const initialState = () => ({
+    field: "",
+    login: "",
+    repos: 0,
+    followers: 0,
+});
+
+export default initialState;
--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -1,7 +1,20 @@
-import {} from './actions/actions';
+import { SET_INFO, FIELD_CHANGE } from '../actions/actions';
+import initialState from './initialState';
 
-const reducer = (state, action) => {
+const reducer = (state = initialState(), action) => {
     switch(action.type){
+        case SET_INFO:
+            return {
+                ...state,
+                login: action.login,
+                repos: action.repos,
+                followers: action.followers,
+            };
+        case FIELD_CHANGE:
+            return {
+                ...state,
+                field: action.value
+            };
         default:
             return state;
     }
